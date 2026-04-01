55239b97676fdff2f48ab066c7a7786d352574cf
Cyrus <rycen81@gmail.com>
2026-04-01 22:17:35 +0800

    Add PostreSQL connection test

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
+
 const authRoutes = require("./routes/authRoutes");
 const photoRoutes = require("./routes/photoRoutes");
-const fs = require("fs");
+const pool = require("./db");
 
 dotenv.config();
 
@@ -27,6 +29,21 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/photos", photoRoutes);
 
+app.get("/api/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      time: result.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
 app.get("/download/:filename", (req, res) => {
   const filePath = path.join(__dirname, "uploads", req.params.filename);
 
@@ -39,6 +56,6 @@ app.get("/download/:filename", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
